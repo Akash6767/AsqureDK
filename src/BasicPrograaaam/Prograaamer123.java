228829bb73efe6eb676614213605e6d0b5ed7124
Akash Kamble <kambleakash679@gmail.com>
2022-07-04 18:02:11 +0530

    first commit

---
+++ src/BasicPrograaaam/Prograaamer123.java
@@ -0,0 +1,12 @@
+package BasicPrograaaam;
+
+public class Prograaamer123 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to The HOME");
+		// TODO Auto-generated method stub
+
+	}
+
+}
